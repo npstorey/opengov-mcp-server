8a1c5b36e60ee4a5ea8640fa6146a9bcb14a046c
npstorey <npstorey@users.noreply.github.com>
2025-05-08 11:46:16 -0400

    Create global.d.ts

---
+++ src/global.d.ts
@@ -0,0 +1,4 @@
+// src/global.d.ts
+declare module '@modelcontextprotocol/sdk/server/index.js';
+declare module '@modelcontextprotocol/sdk/server/http.js';
+declare module '@modelcontextprotocol/sdk/types.js';
